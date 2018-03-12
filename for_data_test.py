61845f46b39bae583460ed11d5b11b38d9338846
JINJINCHEN\chenjinjin <2714893817@qq.com>
2018-03-12 21:15:50 +0800

    msg

--- for_data_test.py
+++ for_data_test.py
@@ -2,13 +2,13 @@ import cv2
 from matplotlib import pyplot as plt
 
 img2 = cv2.imread('test_image\\original_images\\iphone\\85.jpg')
-
-
-cv2.rectangle(img2, (500, 500), (800, 800), (255, 0, 0), 1, 8, 0)
+cv2.rectangle(img2, (500, 500), (800, 800), (255, 255, 0), 3)
 cv2.imwrite("test_rec.jpg", img2)
+
 img2 = img2[500:800, 500:800]
 cv2.imwrite("test_cut.jpg", img2)
+
 img3 = cv2.resize(img2, (100, 100), interpolation=cv2.INTER_CUBIC)
 cv2.imwrite("test_resize.jpg", img3)
-plt.imshow(img3)
+plt.imshow(img2)
 plt.show()
--- preprocess_1.py
+++ preprocess_1.py
@@ -64,7 +64,7 @@ print(src_pts)
 print("dst_pts")
 print(dst_pts)
 # Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in range(len(matches))]
+"""matchesMask = [[0,0] for i in range(len(matches))]
 
 # ratio test as per Lowe's paper
 for i,(m,n) in enumerate(matches):
@@ -74,10 +74,18 @@ draw_params = dict(matchColor=(0,255,0),
                    singlePointColor=(255,0,0),
                    matchesMask=matchesMask,
                    flags=0)
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
-print()
-cv2.rectangle(img3, (np.min(src_pts), np.min(dst_pts)),
-               (np.max(src_pts), np.max(dst_pts)),
-               (255, 0, 0), 1, 8, 0) #(255, 0, 0)是红色
-cv2.imwrite("test.jpg", img3)
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)"""
+print(np.min(src_pts, axis=1)[0][0])
+print(np.min(src_pts, axis=1)[0][1])
+print(np.max(src_pts, axis=1)[0][0])
+print(np.max(src_pts, axis=1)[0][1])
+# 改为分别在两张图中画框
+cv2.rectangle(img1, (np.min(src_pts, axis=1)[0][0], np.min(src_pts, axis=1)[0][1]),
+               (np.max(src_pts, axis=1)[0][0], np.max(src_pts, axis=1)[0][1]),
+               (255, 255, 0), 3) #(255, 255, 0)是huang色,3是线的宽度
+cv2.rectangle(img2, (np.min(dst_pts, axis=1)[0][0], np.min(dst_pts, axis=1)[0][1]),
+               (np.max(dst_pts, axis=1)[0][0], np.max(dst_pts, axis=1)[0][1]),
+               (255, 255, 0), 3) #(255, 255, 0)是huang色,3是线的宽度
+cv2.imwrite("test_1.jpg", img1)
+cv2.imwrite("test_2.jpg", img2)
 
