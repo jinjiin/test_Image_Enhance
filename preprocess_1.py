1234b93e990a184d6806a3ab2e744fb1daa6593d
JINJINCHEN\chenjinjin <2714893817@qq.com>
2018-03-13 15:00:44 +0800

    msg

--- preprocess_1.py
+++ preprocess_1.py
@@ -25,30 +25,46 @@ Flann_index_kdtree = 0
 index_params = dict(algorithm=Flann_index_kdtree, trees=5)
 search_params = dict(checks=50)
 
-src_pts = np.float32([kp1[i].pt for i in range(len(kp1))]).reshape(-1, 1, 2)
-dst_pts = np.float32([kp2[i].pt for i in range(len(kp2))]).reshape(-1, 1, 2)
-print("src_pts")
-print(src_pts)
-print("dst_pts")
-print(dst_pts)
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-matchesMask = mask.ravel().tolist()
+# FLANN 是快速最近邻搜索包
+# KTreeIndex配置索引，指定待处理核密度树的数量（理想的数量在1-16）
+# search_params用它来指定递归遍历的次数。值越高结果越准 确, 但是消耗的时间也越多
+# 5kd-trees和50checks总能取得合理精度，而且短时间完成
 
-h, w = img1.shape
-pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
-dst = cv2.perspectiveTransform(pts, M)
-img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-cv2.imwrite("test_1.jpg", img2)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+print("--------------------------matches--------------------------")
+print(matches)
 
+#store all the good matches as per Lowe's ratio test
+"""good = []
+for m, n in matches:
+    if m.distance < 0.7*n.distance: # 最优匹配距离/次优匹配距离<0.7，即视为是匹配点
+        good.append(m)
+if len(good)>Min_match_count:
+    # get coordinates of keypoints
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h, w = img1.shape
+    pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+    dst = cv2.perspectiveTransform(pts, M)
+    img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+else:
+    print("Not enough matched are found %d / %d" %(len(good), Min_match_count))
+    matchesMask = None
+"""
 # get coordinates of keypoints
-"""src_pts = np.float32([kp1[m.queryIdx].pt for m,n in matches]).reshape(-1, 1, 2)
+src_pts = np.float32([kp1[m.queryIdx].pt for m,n in matches]).reshape(-1, 1, 2)
 dst_pts = np.float32([kp2[m.trainIdx].pt for m,n in matches]).reshape(-1, 1, 2)
 print("src_pts")
 print(src_pts)
 print("dst_pts")
 print(dst_pts)
 # Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in range(len(matches))]
+"""matchesMask = [[0,0] for i in range(len(matches))]
 
 # ratio test as per Lowe's paper
 for i,(m,n) in enumerate(matches):
@@ -58,7 +74,7 @@ draw_params = dict(matchColor=(0,255,0),
                    singlePointColor=(255,0,0),
                    matchesMask=matchesMask,
                    flags=0)
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)"""
 print(np.min(src_pts, axis=1))
 print(np.min(src_pts, axis=1)[0][0])  # 1508.54
 print(np.min(src_pts, axis=1)[0][1])  # 2.6525
@@ -73,4 +89,4 @@ cv2.rectangle(img2, (np.min(dst_pts, axis=1)[0][0], np.min(dst_pts, axis=1)[0][1
                (np.max(dst_pts, axis=1)[0][0], np.max(dst_pts, axis=1)[0][1]),
                (255, 255, 0), 3) #(255, 255, 0)是huang色,3是线的宽度
 cv2.imwrite("test_1.jpg", img1)
-cv2.imwrite("test_2.jpg", img2)"""
+cv2.imwrite("test_2.jpg", img2)
