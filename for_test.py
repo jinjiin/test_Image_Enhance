e2b43c3306aed2550bc4a9183409f903e332e25d
JINJINCHEN\chenjinjin <2714893817@qq.com>
2018-03-14 19:46:04 +0800

    msg

---
+++ for_test.py
@@ -0,0 +1,15 @@
+import multiprocessing as mp
+import time
+
+x = [1,2,3,4,5,6]
+y = [1,1,1,1,1,1]
+x_y = zip(x, y)
+def work(a):
+    print(a+8)
+
+
+if __name__=="__main__":
+    p = mp.Pool(processes=4)
+    p.map_async(work, x)
+    p.close()
+    p.join()
