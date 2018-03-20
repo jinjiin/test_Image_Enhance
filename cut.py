698269d867c5a5b71c29f4d76d7f537802d8ebd2
JINJINCHEN\chenjinjin <2714893817@qq.com>
2018-03-20 17:26:30 +0800

    update

--- cut.py
+++ cut.py
@@ -35,8 +35,9 @@ def cut():
             num = num + 1
 
 
-img = cv2.imread('D:/lena.jpg')
-h = np.zeros((256,256,3)) #创建用于绘制直方图的全0图像
+img = cv2.imread('cut_images\\canon\\63.jpg', 0)
+img2 = cv2.imread('cut_images\\iphone\\63.jpg', 0)
+"""h = np.zeros((256,256,3)) #创建用于绘制直方图的全0图像
 bins = np.arange(256).reshape(256,1) #直方图中各bin的顶点位置
 color = [(255,0,0),(0,255,0),(0,0,255)] #BGR三种颜色
 for ch, col in enumerate(color):
@@ -47,4 +48,17 @@ for ch, col in enumerate(color):
     cv2.polylines(h,[pts],False,col)
     h=np.flipud(h)
     cv2.imshow('colorhist',h)
-    cv2.waitKey(0)
+    cv2.waitKey(0)"""
+"""result = cv2.calcHist([img],
+    [0], #使用的通道
+    None, #没有使用mask
+    [256], #HistSize
+    [0.0,255.0]) #直方图柱的范围
+print(result)
+result2 = cv2.calcHist([img2],
+    [0], #使用的通道
+    None, #没有使用mask
+    [256], #HistSize
+    [0.0,255.0]) #直方图柱的范围
+print('---------------------')
+print(result2)"""
