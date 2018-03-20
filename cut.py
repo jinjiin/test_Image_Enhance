04bd13d83de3686a9626d481f0388722024c77d5
JINJINCHEN\chenjinjin <2714893817@qq.com>
2018-03-20 20:14:22 +0800

    update

--- cut.py
+++ cut.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*
 import cv2
 import numpy as np
+from scipy import signal
+from scipy import misc
 
 def cut():
     img2 = cv2.imread('cut_images\\cut_1.jpg')
@@ -35,30 +38,47 @@ def cut():
             num = num + 1
 
 
-img = cv2.imread('cut_images\\canon\\63.jpg', 0)
-img2 = cv2.imread('cut_images\\iphone\\63.jpg', 0)
-"""h = np.zeros((256,256,3)) #创建用于绘制直方图的全0图像
-bins = np.arange(256).reshape(256,1) #直方图中各bin的顶点位置
-color = [(255,0,0),(0,255,0),(0,0,255)] #BGR三种颜色
-for ch, col in enumerate(color):
-    originHist = cv2.calcHist([img],[ch],None,[256],[0,256])
-    cv2.normalize(originHist, originHist,0,255*0.9,cv2.NORM_MINMAX)
-    hist=np.int32(np.around(originHist))
-    pts = np.column_stack((bins,hist))
-    cv2.polylines(h,[pts],False,col)
-    h=np.flipud(h)
-    cv2.imshow('colorhist',h)
-    cv2.waitKey(0)"""
-"""result = cv2.calcHist([img],
-    [0], #使用的通道
-    None, #没有使用mask
-    [256], #HistSize
-    [0.0,255.0]) #直方图柱的范围
-print(result)
-result2 = cv2.calcHist([img2],
-    [0], #使用的通道
-    None, #没有使用mask
-    [256], #HistSize
-    [0.0,255.0]) #直方图柱的范围
-print('---------------------')
-print(result2)"""
+def rotation():
+    img = cv2.imread('cut_images\\canon\\63.jpg', 0)
+    img2 = cv2.imread('cut_images\\iphone\\63.jpg', 0)
+    h = np.zeros((256, 256, 3))  # 创建用于绘制直方图的全0图像
+    bins = np.arange(256).reshape(256, 1)  # 直方图中各bin的顶点位置
+    color = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]  # BGR三种颜色
+    for ch, col in enumerate(color):
+        originHist = cv2.calcHist([img], [ch], None, [256], [0, 256])
+        cv2.normalize(originHist, originHist, 0, 255 * 0.9, cv2.NORM_MINMAX)
+        hist = np.int32(np.around(originHist))
+        pts = np.column_stack((bins, hist))
+        cv2.polylines(h, [pts], False, col)
+        h = np.flipud(h)
+        cv2.imshow('colorhist', h)
+        cv2.waitKey(0)
+
+
+def rotation(img):
+    result = cv2.calcHist([img],
+                          [0],  # 使用的通道
+                          None,  # 没有使用mask
+                          [256],  # HistSize
+                          [0.0, 255.0])  # 直方图柱的范围
+    print(result)
+    result2 = cv2.calcHist([img2],
+                           [0],  # 使用的通道
+                           None,  # 没有使用mask
+                           [256],  # HistSize
+                           [0.0, 255.0])  # 直方图柱的范围
+    print('---------------------')
+    print(result2)
+
+def NCC():
+    img1 = cv2.imread('cut_images/cut_1.jpg', 0)
+    img2 = cv2.imread('cut_images/cut_2.jpg', 0)
+    corr = signal.correlate2d(img1, img2, boundary='fill', mode='same')
+    print(corr)
+    print(type(img1))
+    print(img1.shape)
+    print(img1[0][0])
+    print(img1[50][50])
+if __name__ == '__main__':
+    NCC()
+
