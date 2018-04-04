7af869ddfa3971680ba9b3308b40508acfbfbf70
jinjiin <2714893817@qq.com>
2018-04-04 19:51:33 +0800

    update

--- pre_FFT.py
+++ pre_FFT.py
@@ -19,20 +19,24 @@ def compare(img1, img2):
     print(diff)
 def FFT(img):
     # 打开图像文件并获取数据
-    srcIm = Image.open(img)
+    #srcIm = Image.open(img)
+    srcIm = cv2.imread(img)
     print(srcIm) # PIL.JpegImagePlugin.JpegImageFile image mode=RGB size=2592x1944 at 0xB39CE1EC18
-    srcArray = np.fromstring(srcIm.tobytes(), dtype=np.int8)
-    print(type(srcArray))  # <class 'numpy.ndarray'>
+    #srcArray = np.fromstring(srcIm.tobytes(), dtype=np.int8)
+    #print(type(srcArray))  # <class 'numpy.ndarray'>
     # 傅里叶变换并滤除低频信号
+    print("srcIm")
+    print(srcIm)
+    print(srcIm.shape)
     result = fft(srcIm)  # result.shape=(1944, 2592, 3)
     # result = np.where(np.absolute(result) < 9e4, 0, result)
     # 傅里叶反变换,保留实部
     result = ifft(result)  # result.shape=(1944, 2592, 3)
     result = np.int8(np.real(result))
     # 转换为图像
-    """im = Image.frombytes(srcIm.mode, srcIm.size, result)
+    im = Image.frombytes('RGB', (srcIm.shape[1], srcIm.shape[0]), result)
     im.show()
-    im.save('C:\\Users\\chenjinjin\\Desktop\\test_image\\50_ufft_2.jpg')"""
+    im.save('C:\\Users\\chenjinjin\\Desktop\\test_image\\50_ufft_#.jpg')
 if __name__ == '__main__':
     FFT('C:\\Users\\chenjinjin\\Desktop\\test_image\\50.jpg')
     #compare('C:\\Users\\chenjinjin\\Desktop\\test_image\\50.jpg', 'C:\\Users\\chenjinjin\\Desktop\\test_image\\50_ufft_2.jpg')
